f0c99d3382e8ff7cbf63cfd869694efa006cd233
us19861229c <rcks.santos@gmail.com>
2020-07-15 22:51:29 -0300

    Exercícios bloco 4, dia 3.

---
+++ 4_3/1-nasteriscosquad.js
@@ -0,0 +1,8 @@
+// Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. 
+
+let n = 5; 
+
+for( let i = 1; i <= n; i = i + 1){
+  let asterisco = "*"
+  console.log(asterisco.repeat(5))
+}
