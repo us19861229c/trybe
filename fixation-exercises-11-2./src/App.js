fe419d1c5238a1cd27965782e842172a27174c97
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-13 19:34:13 -0300

    parte2 exercício pré-aula

--- fixation-exercises-11-2./src/App.js
+++ fixation-exercises-11-2./src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import Image from './Image';
+import UserProfile from './UserProfile'
 
 function App() {
-  const imageInfo = {
+  
+  const joao = {
+    id: 102,
+    name: "João",
+    email: "joao@gmail.com",
+    src: "https://cdn.pixabay.com/photo/2016/08/20/05/38/src-1606916_640.png",
+    altTxt: 'Joao'
+  };
+
+  const amelia = {
+    id: 77,
+    name: "Amélia",
+    email: "amelia@gmail.com",
+    src: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png",
+    altTxt: 'Amelia'
+  };
+
+  const gato = {
+    id: 23,
+    name: "Gato",
+    email: 'emaildegato@gmail.com',
     src: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
-    alternativeText: 'Cute cat staring'
+    altTxt: 'Cute cat staring'
   };
   return (
-   <Image src={imageInfo.src} alt={imageInfo.alternativeText}/>
+    <div>
+      <UserProfile user={gato}/>
+      <UserProfile user={joao}/>
+      <UserProfile user={amelia}/>
+    </div>
   );  
 }
 
